e1f682c626bea32ef8686a31548f631bb92eb313
snowgoing <snowgoing@gmail.com>
2016-03-27 23:14:04 -0700

    First Stage Functional

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 $(document).ready(function(){
+	var right = 0;
+	var wrong = 0;
+
+
 	color();
 	
 	$('.circle1').click(function(e){
-		var choiceRGB = $(this).css('background-color');
+		var choiceRGB = 1;
 		console.log(choiceRGB);
+		hexCheck(choiceRGB);
 	})
 	$('.circle2').click(function(e){
-		var choiceRGB = $(this).css('background-color');
+		var choiceRGB = 2;
 		console.log(choiceRGB);
+		hexCheck(choiceRGB);
 	})
 	$('.circle3').click(function(e){
-		var choiceRGB = $(this).css('background-color');
+		var choiceRGB = 3;
 		console.log(choiceRGB);
+		hexCheck(choiceRGB);
+	})
+	$('.hint').click(function(e){
+		hint();
 	})
 
 
@@ -39,64 +49,59 @@ $(document).ready(function(){
 
 		var allCircles = [circle1, circle2, circle3];
 		var b = allCircles[Math.round(Math.random() * 2)]
+		var c = allCircles.indexOf(b) + 1;
 		
 		$('.hex').text(b);
-		hexCheck();
+		$('.match').text(c);	
+	}	
 
-		var wrong = 0;
-		var right = 0;
-		
-		
-		function hexCheck() {
-			$('.circle1').click(function(){
-				if(circle1 === b){
-					right += 1;
-					$(".right").text(right);
-				} else {
-					wrong += 1;
-					$(".wrong").text(wrong);
-				}
 
+	function hexCheck(val) {
+		var match = $('.match').text().trim();
+		console.log(match);
+		if(val == match){
+			right += 1;
+			$('.right').text(right);
 			color();
-			});
-			
-			$('.circle2').click(function(){
-				if(circle2 === b){
-					right += 1;
-					$(".right").text(right);
-				} else {
-					wrong += 1;
-					$(".wrong").text(wrong);
-				}
-			color();
-			});
-			
-			$('.circle3').click(function(){
-				if(circle3 === b){
-					right += 1;
-					$(".right").text(right);
-				} else {
-					wrong += 1;
-					$(".wrong").text(wrong);
-				}
-			
-			color();
-			});
-
-		}
-
-		// function showAnswer() {
-		// 	$(b).addClass("answer")
-		// 	setInterval($(".answer").show()), 4000;
-		// 	$(".answer").hide();
-		// }
-
-		// function removeAnswer(){
-		// 	$('.container').removeClass("answer");
-		// }
+		} else {
+			wrong += 1;
+			$('.wrong').text(wrong);
+			showAnswer();
+		}	
+		clearHint();
 	}
 
-	
+	function showAnswer(){
+			var a = $('.match').text().trim();
+			var b = '.circle' + a;
+			$(b).css('border', '8px solid #F9E239');
+			clearAnswer(b);
+	}
+
+
+	function clearAnswer(val){
+		setTimeout(function() {
+			$(val).css('border', '1px solid #08043E')
+		}, 3000);
+		setTimeout(function(){
+			color();
+		}, 3000);
+	}
+
+	function hint(){
+		var rgbClue1 = $('.circle1').css('background-color');
+		var rgbClue2 = $('.circle2').css('background-color');
+		var rgbClue3 = $('.circle3').css('background-color');
+		$('.rgbHint1').text(rgbClue1);
+		$('.rgbHint2').text(rgbClue2);
+		$('.rgbHint3').text(rgbClue3);
+	}
+		
+	function clearHint(){
+		$('.rgbHint1').text("");
+		$('.rgbHint2').text("");
+		$('.rgbHint3').text("");
+	}
 
 
 
